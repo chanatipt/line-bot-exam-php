49fa0d838c2e607c38ba7235dd5703ed3ac3aaa8
Chanatip Tippayakul <chanatipt@gmail.com>
2018-08-18 08:12:24 +0700

    test

--- subscribe.php
+++ subscribe.php
@@ -1,12 +1,14 @@
 <?php
 
-require("phpMQTT.php");
+include 'phpMQTT.php'; 
+require "vendor/autoload.php";
+
 $server = "broker.hivemq.com";     // change if necessary
 $port = 1883;                     // change if necessary
 $username = "";                   // set your username
 $password = "";                   // set your password
 $client_id = "phpMQTT-subscriber"; // make sure this is unique for connecting to sever - you could use uniqid()
-$mqtt = new phpMQTT($server, $port, $client_id);
+$mqtt = new Bluerhinos\phpMQTT($server, $port, $client_id);
 if(!$mqtt->connect(true, NULL, $username, $password)) {
 	exit(1);
 }
@@ -20,21 +22,17 @@ function procmsg($topic, $msg){
 		echo "Msg Recieved: " . date("r") . "\n";
 		echo "Topic: {$topic}\n\n";
 		echo "\t$msg\n\n";
-}
-/*
 $access_token = 'W7uUjdWdAR5rlMAhTCHZ11ESL1m/amYYEaMsvoFpy6Y8KcqL19qJp7sb/pGWiLqtSlgd+udUui8LBYAvaeds+YnHozApjfeoTH9kDhbdA3Y+vwaabNcbIhAKv/aR8EbuDe5JqkiYk+at/grNx9ERHgdB04t89/1O/w1cDnyilFU=';
-
 $channelSecret = '06e34e972681b7ad4b6431475c81f9c6';
-
 $pushID = 'U2169edceae217410b46368e5eb96297e';
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($msg);
 $response = $bot->pushMessage($pushID, $textMessageBuilder);
 
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
-*/
+}
 
 ?>
